2fdab3188b7d87b55d16f3d98f95a32cc4df5075
Odhiambo254 <odhiamboandrew76@gmail.com>
2024-03-18 08:18:54 +0300

    data structures in python

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,7 @@
 def replace_in_list(my_list, idx, element):
     my_list_len = len(my_list)
     if idx < 0 or idx > my_list_len - 1:
-        return my_list_len
+        return my_list
     else:
-        return my_list[element]
+        my_list[idx] = element
+        return my_list
